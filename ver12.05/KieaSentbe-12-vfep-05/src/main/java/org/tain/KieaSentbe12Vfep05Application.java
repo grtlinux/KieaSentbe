92c4ffab00ded2d8a2898079e4fad0228e519d57
grtlinux <grtlinux@gmail.com>
2020-09-23 22:54:16 +0900

    set banner.txt

--- ver12.05/KieaSentbe-12-vfep-05/src/main/java/org/tain/KieaSentbe12Vfep05Application.java
+++ ver12.05/KieaSentbe-12-vfep-05/src/main/java/org/tain/KieaSentbe12Vfep05Application.java
@@ -1,13 +1,135 @@
 package org.tain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tain.utils.CurrentInfo;
+import org.tain.utils.Flag;
+import org.tain.working.properties.PropertiesWorking;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-public class KieaSentbe12Vfep05Application {
+@Slf4j
+public class KieaSentbe12Vfep05Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get(), LocalDateTime.now());
 		SpringApplication.run(KieaSentbe12Vfep05Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) job01();
+		if (Flag.flag) job02();
+		if (Flag.flag) job03();
+		if (Flag.flag) job04();
+		if (Flag.flag) job05();
+		if (Flag.flag) job06();
+		if (Flag.flag) job07();
+		if (Flag.flag) job08();
+		if (Flag.flag) job09();
+		if (Flag.flag) job10();
+		
+		if (Flag.flag) System.exit(0);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	@Autowired
+	private PropertiesWorking propertiesWorking;
+	
+	private void job01() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+			this.propertiesWorking.print();
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job02() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job03() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job04() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job05() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job06() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job07() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job08() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job09() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	private void job10() {
+		log.info("KANG-20200923 >>>>> {} {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+		}
+	}
 }
