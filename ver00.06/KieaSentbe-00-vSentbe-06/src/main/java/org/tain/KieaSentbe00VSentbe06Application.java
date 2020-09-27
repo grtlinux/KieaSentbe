3e068cf0f81bb1ca490bff8770c6b163cccb4456
grtlinux <grtlinux@gmail.com>
2020-09-27 17:02:15 +0900

    create vSentbe

---
+++ ver00.06/KieaSentbe-00-vSentbe-06/src/main/java/org/tain/KieaSentbe00VSentbe06Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaSentbe00VSentbe06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaSentbe00VSentbe06Application.class, args);
+	}
+
+}
