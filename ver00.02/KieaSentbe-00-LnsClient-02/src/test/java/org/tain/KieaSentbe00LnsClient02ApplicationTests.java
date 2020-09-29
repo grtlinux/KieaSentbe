c5cec8ddc34e858e8edcee1e56ca65579415ba4e
grtlinux <grtlinux@gmail.com>
2020-09-29 14:56:53 +0900

    create 1st

---
+++ ver00.02/KieaSentbe-00-LnsClient-02/src/test/java/org/tain/KieaSentbe00LnsClient02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaSentbe00LnsClient02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
