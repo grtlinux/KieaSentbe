98ecb918b8c5d11c3c5e598f5fd7be644f072923
grtlinux <grtlinux@gmail.com>
2020-10-25 21:59:14 +0900

    create blank of vfep

---
+++ ver12.06/KieaSentbe-12-vfep-06/src/main/java/org/tain/KieaSentbe12Vfep06Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaSentbe12Vfep06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaSentbe12Vfep06Application.class, args);
+	}
+
+}
