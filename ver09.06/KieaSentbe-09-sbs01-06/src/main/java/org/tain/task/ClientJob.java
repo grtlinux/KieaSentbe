f6749c2e7ac056828872b0b633f9e0be514b7f16
grtlinux <grtlinux@gmail.com>
2020-11-01 18:52:03 +0900

    create 1st

---
+++ ver09.06/KieaSentbe-09-sbs01-06/src/main/java/org/tain/task/ClientJob.java
@@ -0,0 +1,88 @@
+package org.tain.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.tain.object.lns.LnsStream;
+import org.tain.object.ticket.LnsInfoTicket;
+import org.tain.object.ticket.LnsSocketTicket;
+import org.tain.queue.InfoTicketReadyQueue;
+import org.tain.queue.LnsQueueObject;
+import org.tain.queue.LnsSendQueue;
+import org.tain.queue.SocketTicketReadyQueue;
+import org.tain.queue.SocketTicketUseQueue;
+import org.tain.utils.CurrentInfo;
+import org.tain.utils.Flag;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ClientJob {
+
+	private final String TITLE = "CLIENT_JOB ";
+	
+	@Autowired
+	private SocketTicketUseQueue socketTicketUseQueue;
+	
+	@Autowired
+	private SocketTicketReadyQueue socketTicketReadyQueue;
+	
+	@Autowired
+	private InfoTicketReadyQueue infoTicketReadyQueue;
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	@Autowired
+	private LnsSendQueue lnsSendQueue;
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	@Async(value = "clientTask")
+	public void clientJob(LnsInfoTicket infoTicket) throws Exception {
+		log.info(TITLE + ">>>>> START param = {}, {}", infoTicket, CurrentInfo.get());
+		
+		LnsSocketTicket lnsSocketTicket = null;
+		if (Flag.flag) {
+			lnsSocketTicket = this.socketTicketUseQueue.get();  // blocking
+			log.info(TITLE + ">>>>> clientJob: INFO = {} {}", infoTicket, lnsSocketTicket);
+		}
+		
+		////////////////////////////////////////////////////
+		if (Flag.flag) {
+			LnsQueueObject lnsQueueObject = null;
+			try {
+				LnsStream reqLnsStream = null;
+				LnsStream resLnsStream = null;
+				while (true) {
+					// from SendQueue
+					lnsQueueObject = (LnsQueueObject) this.lnsSendQueue.get();
+					
+					// send
+					reqLnsStream = lnsQueueObject.getLnsStream();
+					lnsSocketTicket.sendStream(reqLnsStream);
+					
+					// recv
+					resLnsStream = lnsSocketTicket.recvStream();
+					lnsQueueObject.getLnsRecvQueue().set(resLnsStream);
+				}
+			} catch (Exception e) {
+				//e.printStackTrace();
+				// ERROR >>>>> ERROR: return value of read is negative(-)...
+				log.error(TITLE + " ERROR >>>>> {}", e.getMessage());
+				if (lnsQueueObject != null)
+					this.lnsSendQueue.set(lnsQueueObject);
+			} finally {
+				lnsSocketTicket.close();
+			}
+		}
+		
+		if (Flag.flag) {
+			// return the tickets.
+			this.socketTicketReadyQueue.set(lnsSocketTicket);
+			this.infoTicketReadyQueue.set(infoTicket);
+		}
+		
+		log.info(TITLE + ">>>>> END   param = {}, {}", infoTicket, CurrentInfo.get());
+	}
+}
---
+++ ver09.06/KieaSentbe-09-sbs01-06/src/main/java/org/tain/task/Task01Job.java
@@ -0,0 +1,59 @@
+package org.tain.task;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.tain.object.lns.LnsStream;
+import org.tain.queue.LnsQueueObject;
+import org.tain.queue.LnsRecvQueue;
+import org.tain.queue.LnsSendQueue;
+import org.tain.utils.CurrentInfo;
+import org.tain.utils.Flag;
+import org.tain.utils.JsonPrint;
+import org.tain.utils.Sleep;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class Task01Job {
+
+	private final String TITLE = "TASK01_JOB ";
+	
+	@Autowired
+	private LnsSendQueue lnsSendQueue;
+	
+	private LnsRecvQueue lnsRecvQueue = new LnsRecvQueue();
+	
+	@Async(value = "task01Task")
+	public void task01Job(String param) throws Exception {
+		log.info(TITLE + ">>>>> START param = {}, {}", param, CurrentInfo.get());
+		
+		
+		if (Flag.flag) {
+			// single
+			for (int i=0; ; i++) {
+				if (Flag.flag) Sleep.run(10 * 1000);
+				
+				String reqStrData = String.format("REQ-[%04d]  Hello, Server!!! %s", i, LocalDateTime.now());
+				String reqTypeCode = "0700100";
+				String reqLen = String.format("%04d", 7 + reqStrData.length());
+				LnsStream reqLnsStream = new LnsStream(reqLen + reqTypeCode + reqStrData);
+				
+				LnsQueueObject reqLnsQueueObject = LnsQueueObject.builder()
+						.lnsStream(reqLnsStream)
+						.lnsRecvQueue(this.lnsRecvQueue)
+						.build();
+				if (Flag.flag) System.out.println(">>>>> REQ: LnsStream: " + JsonPrint.getInstance().toPrettyJson(reqLnsStream));
+				this.lnsSendQueue.set(reqLnsQueueObject);
+				
+				LnsStream resLnsStream = (LnsStream) this.lnsRecvQueue.get();
+				if (Flag.flag) System.out.println(">>>>> RES: LnsStream: " + JsonPrint.getInstance().toPrettyJson(resLnsStream));
+			}
+		}
+		
+		log.info(TITLE + ">>>>> END   param = {}, {}", param, CurrentInfo.get());
+	}
+}
