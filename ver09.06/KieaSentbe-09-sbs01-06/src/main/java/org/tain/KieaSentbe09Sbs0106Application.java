e43c99cd8ef6a040069d7c8826d3fbbd12da415a
grtlinux <grtlinux@gmail.com>
2020-11-01 18:42:39 +0900

    create 1st

---
+++ ver09.06/KieaSentbe-09-sbs01-06/src/main/java/org/tain/KieaSentbe09Sbs0106Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaSentbe09Sbs0106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaSentbe09Sbs0106Application.class, args);
+	}
+
+}
---
+++ ver09.06/KieaSentbe-09-sbs01-06/src/test/java/org/tain/KieaSentbe09Sbs0106ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaSentbe09Sbs0106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
